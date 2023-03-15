751a3908cf082cd9dde21da67d55f6286fa05e82
Daniel Caresma Silva <91802662+DanielCaresma@users.noreply.github.com>
2023-03-15 16:53:40 -0300

    Criando validação dos formularios

---
+++ assets/js/validaFormulario.js
@@ -0,0 +1,61 @@
+const camposDoFormulario = document.querySelectorAll("[required]")
+const fromulario = document.querySelector("[data-formulario]")
+// const mensagemErro = inputEmail.parentNode.querySelector(".mensagem-erro")
+
+
+
+const tiposDeErro = [
+    'valueMissing',
+    'typeMismatch',
+    'patternMismatch',
+    'tooShort',
+    'tooLong'
+]
+
+const mensagens = {
+    nome: {
+        valueMissing: "O campo de nome não pode estar vazio.",
+        tooShort: "Por favor, preencha um nome válido."
+    },
+    email: {
+        valueMissing: "O campo de e-mail não pode estar vazio.",
+        typeMismatch: "Por favor, preencha um email válido.",
+    },
+    senha: {
+        valueMissing: 'O campo de senha não pode estar vazio',
+        tooShort: "A senha deve conter no mínimo 6 caracteres",
+        patternMismatch: "A senha deve conter pelo menos um número, uma letra e um caracter especial",
+    }
+}
+
+
+fromulario.addEventListener("submit", evento => {
+    evento.preventDefault()
+
+})
+
+camposDoFormulario.forEach(campo => {
+    campo.addEventListener("blur", () => verificaCampo(campo))
+    campo.addEventListener("invalid", evento => evento.preventDefault())
+})
+
+function verificaCampo(campo) {
+    let mensagem = ""
+    tiposDeErro.forEach(erro => {
+
+        if (campo.validity[erro]) {
+            mensagem = mensagens[campo.name][erro]
+        }
+    })
+
+    const mensagemErro = campo.parentNode.querySelector('.mensagem-erro')
+    const validadorDeInput = campo.checkValidity()
+    console.log(validadorDeInput)
+
+    if (!validadorDeInput) {
+        mensagemErro.textContent = mensagem
+    } else {
+        mensagemErro.textContent = ""
+    }
+}
+
