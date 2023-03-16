2e485c60fc251e0761c607be88729f68f9f79d13
Daniel Caresma Silva <91802662+DanielCaresma@users.noreply.github.com>
2023-03-16 17:29:39 -0300

    Corrigindo mensagem de erro

--- assets/js/confirmaSenha.js
+++ assets/js/confirmaSenha.js
@@ -3,7 +3,7 @@ export default function confirmaSenha(campo) {
     const senhaReptida = campo.value
     
     if (!(senha == senhaReptida)){
-        campo.setCustomValidity('Este campo não corresponde a senha criada anteriormente!')
+        campo.setCustomValidity('Este campo não corresponde com a senha criada anteriormente!')
     }    
 }
 
