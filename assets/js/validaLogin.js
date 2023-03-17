866a6a2f9c3b6dc5801acf110e396a531dcd09af
Daniel Caresma Silva <91802662+DanielCaresma@users.noreply.github.com>
2023-03-17 13:11:02 -0300

    Criando validação de senha da tela de login

--- assets/js/validaLogin.js
+++ assets/js/validaLogin.js
@@ -5,21 +5,31 @@ const camposDoFormulario = document.querySelectorAll("[required]")
 
 camposDoFormulario.forEach(campo => {
     campo.addEventListener("blur", () => validaLogin(campo))
+    campo.addEventListener("invalid", evento => evento.preventDefault())
 })
 
 function validaLogin(campo) {
     let mensagem = ''
     const usuarios = JSON.parse(localStorage.getItem('usuarios'))
 
-    if(campo.name == "email" && !(usuarios.find(usuario => usuario.email == campo.value))) {
-            campo.setCustomValidity('Email não cadastrado')
-            mensagem = 'Email não cadastrado'
-            imprimeErro(campo, mensagem)
+
+    // SE usuarios.find retorna NULL deve entrar na condição
+    if (campo.name == "email" && !(usuarios.find(usuario => usuario.email == campo.value))) {
+        campo.setCustomValidity('Email não cadastrado')
+        mensagem = 'Email não cadastrado'
     }
-    
-    if(campo.name == "senha"){
-        
-    }  
+
+    if (campo.name == "senha") {
+        const email = formulario.elements["email"].value
+        const usuario = usuarios.find(usuario => usuario.email == email)
+
+        if (usuario.senha != campo.value) {
+            campo.setCustomValidity('Senha inválida')
+            mensagem = 'Senha inválida'
+        }
+    }
+
+    imprimeErro(campo, mensagem)
 }
 
 
