71480dd90f9ea2e529fbed84c0537da4099498e8
Daniel Caresma Silva <91802662+DanielCaresma@users.noreply.github.com>
2023-03-17 17:29:40 -0300

    Criando script para exibir e esconder senha

---
+++ assets/js/exibeSenha.js
@@ -0,0 +1,23 @@
+const eye = document.querySelectorAll("[data-eye]")
+
+eye.forEach(eye => {
+
+    eye.addEventListener('click', event => {
+        eye = event.target
+        const inputSenha = eye.parentNode.querySelector('[data-senha]')
+
+        if (inputSenha.type == "password") {
+            inputSenha.type = "text"
+            eye.classList.value = "fa-solid fa-eye icon-eye"
+            eye.style.before.color = "#00aaff"
+            console.log(event.target)
+        } else {
+            inputSenha.type = "password"
+            eye.classList.value = "fa-solid fa-eye-slash icon-eye"
+            console.log(eye.classList)
+        }
+        
+    })
+
+    // <i class="fa-solid fa-eye"></i>
+})
